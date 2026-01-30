8c321f2b39caadc1731df90eac517d48b8a1ae53
Jakob Jilg <167651965+weevil-see@users.noreply.github.com>
2026-01-30 11:29:25 +0100

    Delete site directory

--- site/assets/js/externalURL.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const links = document.querySelectorAll('a[href^="http"]');
-  links.forEach(l => l.setAttribute("target", "_blank"));
-});
